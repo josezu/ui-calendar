5eb2a45686c124deb6c07c8f4171506a1288aa87
Jose Zúñiga <josezu123@gmail.com>
2020-09-08 11:12:49 -0500

    Added env example

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,14 @@
 export const API_WEATHER_URL = process.env.API_WEATHER_URL
   ? process.env.API_WEATHER_URL
   : ''
+export const API_WEATHER_KEY = process.env.API_WEATHER_KEY
+  ? process.env.API_WEATHER_KEY
+  : ''
+
 export const API_CITIES_URL = process.env.API_CITIES_URL
   ? process.env.API_CITIES_URL
+  : ''
+
+export const API_CITIES_KEYL = process.env.API_CITIES_KEY
+  ? process.env.API_CITIES_KEY
   : ''
--- src/data/reminder.ts
+++ src/data/reminder.ts
@@ -1,6 +1,7 @@
 import { Reminder } from '@root/types/reminder'
 
 export const retrieveReminders = () => {
+  //Localstorage instead api service
   const reminders = localStorage.getItem('reminders')
   if (typeof reminders === 'string') {
     return JSON.parse(reminders) as Reminder[]
@@ -9,8 +10,12 @@ export const retrieveReminders = () => {
   }
 }
 export const recordReminders = (reminders: Reminder[]) => {
+  //Localstorage instead api service
+
   window.localStorage.setItem('reminders', JSON.stringify(reminders))
 }
 export const removeReminders = () => {
+  //Localstorage instead api service
+
   window.localStorage.removeItem('reminders')
 }
